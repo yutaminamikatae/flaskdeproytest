edab88e0e0fdb43bf00c72f7e1a107c827654058
yutaminamikatae <toha4heavy4yyyy@gmail.com>
2025-12-06 20:30:25 +0900

    4

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from waitress import server
 
 app = Flask(__name__)
 
+# route()デコレータ
 @app.route("/")
 def index():
-    return "hello"
+    return "-hello-"
+
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=8000)
